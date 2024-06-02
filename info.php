baf7e0bce612398c70429d9dc6641d307ebce6b9
devasish-das <devesish@gmail.com>
2024-06-02 16:44:05 +0600

    learn post method

---
+++ info.php
@@ -0,0 +1,25 @@
+
+
+<?php
+
+
+
+if(isset($_POST["save"]))
+{
+    $a= $_POST["name"];
+    $b= $_POST["uni"];
+
+
+
+
+    echo "<h1>Bruh</h1>";
+    echo "Your Name is $a";
+    echo "<br>";
+    echo "And your universiyt is $b";
+    echo "<br>";
+
+}
+
+
+
+?>
